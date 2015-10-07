8f5f2e1bef74303b1bebfc8f1713ab4becf8a964
Dale Muccignat <dale.muccignat@my.jcu.edu.au>
2015-10-07 13:34:58 +1000

    Made get_details scan through the document for country_name

--- Curreny.py
+++ Curreny.py
@@ -13,13 +13,18 @@ def get_details(country_name):
     document_end = ""
     country_details = ""
     line_count = 0
+    #Format is like: CountryName,Code,Symbol
     currency_details = open("currency_details.txt", mode="r", encoding="utf-8")
+
     while document_end == "":
-        if str(country_name.capitalize()) in currency_details.readline():   #formats the string with .capatilize and checks if in document
+        line = currency_details.readline()
+        if str(country_name.title()) in line:   #formats the string with .capatilize and checks if in document
             document_end = "True"
             currency_details.close()
             currency_details = open("currency_details.txt", mode="r", encoding="utf-8")
             country_details = currency_details.readlines()
+        elif line == "":
+            document_end = "True"
         else:
             line_count += 1
 
@@ -29,5 +34,7 @@ def get_details(country_name):
         print(country_details[line_count])
     currency_details.close()
 
+#Test functions
+# convert(input("Amount to convert: "), input("Convert From: "), input("Convert Too: "))
 get_details(input("Enter Country: "))
 
